39b9f9eb98a5e6b95c27eeaea4d576c02e8e1c7d
huzaifahp7 <huzaifahp7@gmail.com>
2024-03-15 10:53:12 -0400

    numclaim first run

--- src/llama/scripts/numclaim_script.py
+++ src/llama/scripts/numclaim_script.py
@@ -1,4 +1,7 @@
 import pandas as pd 
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
 from time import sleep, time
 from datetime import date
 from together_pipeline import generate
@@ -13,7 +16,8 @@ api_key = "1ba68d2ffcbdad1ac7dbc992797cfa0200a9031ab7c886e6701674892ba4acbf"
 
 # Initialize lists to store actual labels and model responses
 context = []
-llm_responses = []
+llm_responses_label = []
+llm_responses_explanation = []
 complete_responses = []
 actual_labels = []
 
@@ -32,10 +36,11 @@ for i in range(len(dataset['train'])):
         model_response = generate(task, model, api_key, sentence['context'])
         complete_responses.append(model_response)
         response_label = model_response["output"]["choices"][0]["text"]
-        print(response_label)
-        llm_responses.append(response_label)
-        df = pd.DataFrame({'context': context, 'complete_response': complete_responses, 'llm_responses': llm_responses, 'actual_labels': actual_labels})
-        time_taken = int((time() - start_t)/60.0)
+        words = word_tokenize(response_label.strip())
+        llm_responses_label.append(words[0])
+        print(llm_responses_label)
+        llm_responses_explanation.append(response_label)
+        df = pd.DataFrame({'context': context, 'complete_response': complete_responses,'llm_responses_labels': llm_responses_label, 'llm_responses_explanation': llm_responses_explanation, 'actual_labels': actual_labels})
         df.to_csv(f'numclaim_results_llama_2_{today.strftime("%d_%m_%Y")}.csv', index=False)
         
     except Exception as e:
@@ -50,13 +55,14 @@ for i in range(len(dataset['test'])):
     actual_label = sentence['response']  # Assuming the response key contains the actual label
     actual_labels.append(actual_label)
     try:
-        model_response = generate("numclaim", model, api_key, sentence['context'])
+        model_response = generate(task, model, api_key, sentence['context'])
         complete_responses.append(model_response)
         response_label = model_response["output"]["choices"][0]["text"]
-        llm_responses.append(response_label)
-        df = pd.DataFrame({'context': context,'complete_response': complete_responses, 'llm_responses': llm_responses, 'actual_labels': actual_labels})
-        time_taken = int((time() - start_t)/60.0)
-        # sleep(1.0)
+        words = word_tokenize(response_label.strip())
+        llm_responses_label.append(words[0])
+        print(llm_responses_label)
+        llm_responses_explanation.append(response_label)
+        df = pd.DataFrame({'context': context, 'complete_response': complete_responses,'llm_responses_labels': llm_responses_label, 'llm_responses_explanation': llm_responses_explanation, 'actual_labels': actual_labels})
         df.to_csv(f'numclaim_results_llama_2_{today.strftime("%d_%m_%Y")}.csv', index=False)
         
     except Exception as e:
@@ -64,12 +70,16 @@ for i in range(len(dataset['test'])):
         i = i - 1
         sleep(10.0)
         
+time_taken = time() - start_t
+df = pd.DataFrame({'context': context, 'complete_response': complete_responses,'llm_responses_labels': llm_responses_label, 'llm_responses_explanation': llm_responses_explanation, 'actual_labels': actual_labels})
+df.to_csv(f'numclaim_results_llama_2_{today.strftime("%d_%m_%Y")}_{time_taken}.csv', index=False)
+        
 
 # Evaluating metrics for the train split
-accuracy = accuracy_score(actual_labels, llm_responses)
-precision = precision_score(actual_labels, llm_responses, average='micro')
-recall = recall_score(actual_labels, llm_responses, average='micro')
-f1 = f1_score(actual_labels, llm_responses, average='micro')
+accuracy = accuracy_score(actual_labels, llm_responses_label)
+precision = precision_score(actual_labels, llm_responses_label, average='micro')
+recall = recall_score(actual_labels, llm_responses_label, average='micro')
+f1 = f1_score(actual_labels, llm_responses_label, average='micro')
 # roc_auc = roc_auc_score(actual_labels, llm_responses)
 
 
