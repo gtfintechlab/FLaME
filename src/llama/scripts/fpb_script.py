d6ac96449b41b10754827c3684f311e55b187dc9
huzaifahp7 <huzaifahp7@gmail.com>
2024-04-05 06:15:23 -0400

    testing

--- src/llama/scripts/fpb_script.py
+++ src/llama/scripts/fpb_script.py
@@ -4,6 +4,9 @@ from together_pipeline import generate
 from datasets import load_dataset
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from datetime import date
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
 
 today = date.today()
 model = "meta-llama/Llama-2-7b-hf"
@@ -17,6 +20,7 @@ for config in configs:
     # Initialize lists to store actual labels and model responses
     sentences = []
     llm_responses = []
+    llm_first_word_responses = []
     actual_labels = []
     complete_responses = []
 
@@ -31,8 +35,10 @@ for config in configs:
             model_response = generate(task, model, api_key, sentence)
             complete_responses.append(model_response)
             response_label = model_response["output"]["choices"][0]["text"]
+            words = word_tokenize(response_label.strip())
+            llm_first_word_responses.append(words[0])
             llm_responses.append(response_label)
-            df = pd.DataFrame({'sentences': sentences, 'complete_responses': complete_responses, 'llm_responses': llm_responses, 'actual_labels': actual_labels})
+            df = pd.DataFrame({'sentences': sentences, 'complete_responses': complete_responses, 'llm_responses': llm_responses, 'llm_first_word_responses': llm_first_word_responses, 'actual_labels': actual_labels})
             df.to_csv(f'fpb_results_llama_2_{config}_{today.strftime("%d_%m_%Y")}.csv', index=False)
         except Exception as e:
             print(e)
@@ -42,11 +48,11 @@ for config in configs:
     time_taken = time.time() - start_t
 
     # Evaluating metrics for the train split
-    accuracy = accuracy_score(actual_labels, llm_responses)
-    precision = precision_score(actual_labels, llm_responses, average='micro')
-    recall = recall_score(actual_labels, llm_responses, average='micro')
-    f1 = f1_score(actual_labels, llm_responses, average='micro')
-    # roc_auc = roc_auc_score(actual_labels, llm_responses) # Uncomment if applicable
+    accuracy = accuracy_score(actual_labels, llm_first_word_responses)
+    precision = precision_score(actual_labels, llm_first_word_responses, average='micro')
+    recall = recall_score(actual_labels, llm_first_word_responses, average='micro')
+    f1 = f1_score(actual_labels, llm_first_word_responses, average='micro')
+    # roc_auc = roc_auc_score(actual_labels, llm_first_word_responses) # Uncomment if applicable
 
     # Creating DataFrames for metrics
     metrics = pd.DataFrame({'accuracy': [accuracy],
@@ -57,3 +63,4 @@ for config in configs:
 
     # Saving DataFrames to CSV files
     metrics.to_csv(f'fpb_llama2_metrics_{config}_{today.strftime("%d_%m_%Y")}_{time_taken}.csv', index=False)
+
