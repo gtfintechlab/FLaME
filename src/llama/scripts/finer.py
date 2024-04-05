5a26e28cfe5ab10a5456a3a855142bc6735e3a82
huzaifahp7 <huzaifahp7@gmail.com>
2024-04-05 06:39:52 -0400

    testing

--- src/llama/scripts/finer.py
+++ src/llama/scripts/finer.py
@@ -2,89 +2,62 @@
 
 import pandas as pd
 import time
+import nltk
+from nltk.tokenize import word_tokenize
 from together_pipeline import generate
 from datasets import load_dataset
-from sklearn.metrics import (
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score,
-    roc_auc_score,
-)
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from datetime import date
 
+nltk.download('punkt')
 
+today = date.today()
 model = "meta-llama/Llama-2-7b-hf"
 task = "finer"
-dataset = load_dataset(
-    "gtfintechlab/finer_ord_encoded", token=""
-)
-api_key = ""
+dataset = load_dataset("gtfintechlab/finer_ord_encoded", token="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")
+api_key = "1ba68d2ffcbdad1ac7dbc992797cfa0200a9031ab7c886e6701674892ba4acbf"
 
 # Initialize lists to store actual labels and model responses
 context = []
 llm_responses = []
-complete_responses =[]
+llm_first_word_responses = []
+complete_responses = []
 actual_labels = []
 
 # Iterating through the train split of the dataset
-for sentence in dataset["train"]:
-    context.append(sentence["sentence"])
-    actual_label = sentence["label"]
+start_t = time.time()
+for i in range(len(dataset['train'])):
+    sentence = dataset['train'][i]['']
+    context.append(sentence)
+    actual_label = dataset['train'][i]['label']
     actual_labels.append(actual_label)
-    model_response = generate(task, model, api_key, sentence["sentence"])
-    complete_responses.append(model_response)
-    response_label = model_response["output"]["choices"][0]["text"]
-    print(response_label)
-    llm_responses.append(response_label)
-    df = pd.DataFrame(
-        {
-            "context": context,
-            "complete_responses": complete_responses,
-            "response": llm_responses,
-            "actual_label": actual_labels,
-        }
-    )
-    df.to_csv("fomc_llama_2.csv", index=False)
+    try:
+        model_response = generate(task, model, api_key, sentence)
+        complete_responses.append(model_response)
+        response_label = model_response["output"]["choices"][0]["text"]
+        words = word_tokenize(response_label.strip())
+        llm_first_word_responses.append(words[0])
+        llm_responses.append(response_label)
+        df = pd.DataFrame({'context': context, 'complete_responses': complete_responses, 'llm_responses': llm_responses, 'llm_first_word_responses': llm_first_word_responses, 'actual_labels': actual_labels})
+        df.to_csv(f'finer_results_llama_2_{today.strftime("%d_%m_%Y")}.csv', index=False)
+    except Exception as e:
+        print(e)
+        i = i - 1
+        time.sleep(10.0)
 
-# Iterating through the test split of the dataset
-for sentence in dataset["test"]:
-    context.append(sentence["sentence"])
-    actual_label = sentence[
-        "label"
-    ]  # Assuming the response key contains the actual label
-    actual_labels.append(actual_label)
-    model_response = generate("fomc", model, api_key, sentence["sentence"])
-    complete_responses.append(model_response)
-    response_label = model_response["output"]["choices"][0]["text"]
-    llm_responses.append(response_label)
-    df = pd.DataFrame(
-        {
-            "context": context,
-            "complete_responses": complete_responses,
-            "response": llm_responses,
-            "actual_label": actual_labels,
-        }
-    )
-    df.to_csv("fomc_llama_2.csv", index=False)
-
-# Evaluating metrics
-
-accuracy = accuracy_score(actual_labels, llm_responses)
-precision = precision_score(actual_labels, llm_responses)
-recall = recall_score(actual_labels, llm_responses)
-f1 = f1_score(actual_labels, llm_responses)
-roc_auc = roc_auc_score(actual_labels, llm_responses)
+# Evaluating metrics for the train split
+accuracy = accuracy_score(actual_labels, llm_first_word_responses)
+precision = precision_score(actual_labels, llm_first_word_responses, average='micro')
+recall = recall_score(actual_labels, llm_first_word_responses, average='micro')
+f1 = f1_score(actual_labels, llm_first_word_responses, average='micro')
+# roc_auc = roc_auc_score(actual_labels, llm_first_word_responses) # Uncomment if applicable
 
 # Creating DataFrames for metrics
-metrics = pd.DataFrame(
-    {
-        "accuracy": [accuracy],
-        "precision": [precision],
-        "recall": [recall],
-        "f1_score": [f1],
-        "roc_auc": [roc_auc],
-    }
-)
+metrics = pd.DataFrame({'accuracy': [accuracy],
+                        'precision': [precision],
+                        'recall': [recall],
+                        'f1_score': [f1]})
+                        #'roc_auc': [roc_auc]}) # Uncomment if applicable
 
 # Saving DataFrames to CSV files
-metrics.to_csv("fomc_llama2_metrics.csv", index=False)
+metrics.to_csv(f'finer_llama2_metrics_{today.strftime("%d_%m_%Y")}.csv', index=False)
