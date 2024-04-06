212ca4ceb9d0da637ee0300a8f9adb17565f7afd
huzaifahp7 <huzaifahp7@gmail.com>
2024-04-06 18:08:14 -0400

    initial changes to structure

---
+++ src/together/src/fpb/fpb_inference.py
@@ -0,0 +1,92 @@
+import together
+# from utils.prompt_generator import fpb_prompt
+import pandas as pd
+import time
+# from together_pipeline import generate
+from datasets import load_dataset
+from datetime import date
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
+
+def fpb_inference(args):
+    together.api_key = args.api_key
+    today = date.today()
+    # OPTIONAL TODO: make configs an argument of some kind LOW LOW LOW PRIORITY
+    # configs = ["sentences_50agree", "sentences_66agree", "sentences_75agree", "sentences_allagree"]
+    configs = ["sentences_allagree"]
+    for config in configs:
+        dataset = load_dataset("financial_phrasebank", config, token=args.hf_token)
+
+        # Initialize lists to store actual labels and model responses
+        sentences = []
+        llm_responses = []
+        llm_first_word_responses = []
+        actual_labels = []
+        complete_responses = []
+
+        # Iterating through the train split of the dataset
+        start_t = time.time()
+        for i in range(len(dataset['train'])):
+            sentence = dataset['train'][i]['sentence']
+            actual_label = dataset['train'][i]['label']
+            sentences.append(sentence)
+            actual_labels.append(actual_label)
+            prompt = fpb_prompt + "\n\n" + sentence
+            try:
+                model_response = together.Complete.create(prompt=fpb_prompt(sentence),
+                                model=args.model,
+                                max_tokens=args.max_tokens,
+                                temperature=args.temperature,
+                                top_k=args.top_k,
+                                top_p=args.top_p,
+                                repetition_penalty=args.repetition_penalty,
+                                stop=tokens(args.model)
+                                )
+                complete_responses.append(model_response)
+                response_label = model_response["output"]["choices"][0]["text"]
+                words = word_tokenize(response_label.strip())
+                llm_first_word_responses.append(words[0])
+                llm_responses.append(response_label)
+                df = pd.DataFrame({'sentences': sentences, 'complete_responses': complete_responses, 'llm_responses': llm_responses, 'llm_first_word_responses': llm_first_word_responses, 'actual_labels': actual_labels})
+            except Exception as e:
+                print(e)
+                i = i - 1
+                time.sleep(10.0)
+
+        time_taken = time.time() - start_t
+    return df
+####
+
+
+# output = together.Complete.create(
+#     prompt= f"<human>: {prompt} \n<bot>:", # TODO: PROMPT HAS TO COME FROM THE TASK AND MODEL
+#         # STEP1: have a function that takes in the model and returns the special tokens and the prompt function
+#         # STEP2: use the task provided in args to collect the prompt text
+#         # STEP3: use the prompt text on the prompt function associate with that particular model to format the prompt
+#     # model=model,
+#     # max_tokens=256,
+#     # temperature=0.8,
+#     # top_k=60,
+#     # top_p=0.6,
+#     # repetition_penalty=1.1,
+#     stop=["<human>", "\n\n"], # TODO: STOP WORDS SHOULD COME FROM THE MODEL ONLY
+# )
+def fpb_prompt(sentence: str):
+    system_prompt = f''' Discard all the previous instructions. Behave like you are an expert sentence sentiment classifier  '''
+
+    user_msg = f''' Classify the following sentence into ‘NEGATIVE’, ‘POSITIVE’, or ‘NEUTRAL’
+                class. Label ‘NEGATIVE’ if it is corresponding to negative sentiment, ‘POSITIVE’ if it is
+                corresponding to positive sentiment, or ‘NEUTRAL’ if the sentiment is neutral. Provide
+                the label in the first line and provide a short explanation in the second line. This is the sentence: {sentence}'''
+    
+    prompt = f"""<s>[INST] <<SYS>> {system_prompt} <</SYS>> {user_msg} [/INST]"""
+    
+    return prompt
+
+def tokens(model_name):
+    if model_name == "meta-llama/Llama-2-7b-chat-hf":
+        stop = ["<human>", "\n\n"]
+        
+    return stop
---
+++ src/together/src/inference.py
@@ -0,0 +1,127 @@
+import argparse
+from time import time
+from datetime import date
+import pandas as pd
+import together
+from fpb.fpb_inference import fpb_inference
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+)
+
+# from utils.prompt_generator import (
+#     numclaim_prompt,
+#     fomc_prompt,
+#     finer_prompt,
+#     fpb_prompt,
+#     finentity_prompt,
+#     finqa_prompt,
+#     ectsum_prompt,
+#     banking77_prompt,
+#     convfinqa_prompt,
+# )
+
+today = date.today()
+
+# task_prompt_map = {
+#     "numclaim": numclaim_prompt,
+#     "fomc": fomc_prompt,
+#     "finer": finer_prompt,
+#     "fpb": fpb_prompt,
+#     "finentity": finentity_prompt,
+#     "finqa": finqa_prompt,
+#     "ectsum": ectsum_prompt,
+#     "banking77": banking77_prompt,
+#     "convfinqa": convfinqa_prompt,
+# }
+
+task_generation_map = {
+    "fpb": fpb_inference
+    # "numclaim": numclaim_inference,
+    # "fomc": fomc_inference,
+    # "finer": finer_inference,
+    # "finentity": finentity_inference,
+    # "finqa": finqa_inference,
+    # "ectsum": ectsum_inference,
+    # "banking77": banking77_inference,
+    # "convfinqa": convfinqa_inference,
+}
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Run a LLM on TogetherAI over the SuperFLUE dataset"
+    )
+    parser.add_argument("--model", type=str, help="Model to use")
+    parser.add_argument("--task", type=str, help="Task to use")
+    parser.add_argument("--api_key", type=str, help="API key to use")
+    parser.add_argument("--hf_token", type=str, help="Hugging Face token to use")
+    parser.add_argument("--max_tokens", type=int, help="Max tokens to use, default = 128")
+    parser.add_argument("--temperature", type=int, help="Temperature to use")
+    parser.add_argument('--top_p', type=int, help='Top-p to use, default = 0.7')
+    parser.add_argument("--top_k", type=int, help="Top-k to use, default = 50")
+    parser.add_argument('--repetition_penalty', type=int, help='Repetition penalty to use, default = 1.1')
+    args = parser.parse_args()
+    return args
+
+def evaluate(df):
+    # Evaluating metrics for the train split
+    accuracy = accuracy_score(df['actual_labels'], df['llm_first_word_responses'])
+    precision = precision_score(df['actual_labels'], df['llm_first_word_responses'], average='micro')
+    recall = recall_score(df['actual_labels'], df['llm_first_word_responses'], average='micro')
+    f1 = f1_score(df['actual_labels'], df['llm_first_word_responses'], average='micro')
+    # roc_auc = roc_auc_score(actual_labels, llm_first_word_responses) # Uncomment if applicable
+
+    # Creating DataFrames for metrics
+    metrics = pd.DataFrame({'accuracy': [accuracy],
+                            'precision': [precision],
+                            'recall': [recall],
+                            'f1_score': [f1]})
+                            #'roc_auc': [roc_auc]}) # Uncomment if applicable
+
+    return metrics
+
+
+def main():
+    args = parse_arguments()
+    inference_function = task_generation_map[args.task]
+    start_t = time.time()
+    df = inference_function(args)
+    time_taken = time.time() - start_t
+    df.to_csv(
+        f'{args.task}_results_{args.model}_{args.task}_{today.strftime("%d_%m_%Y")}_{time_taken}.csv', index=False
+    )
+    # TODO: CALCULATE METRICS USING DF
+    # model, task = args.model, args.task
+    df_metrics = evaluate(df)
+    df_metrics.to_csv(
+        f'{args.task}_results_{args.model}_{args.task}_{today.strftime("%d_%m_%Y")}_metrics.csv', index=False
+    )
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+####
+
+# # Evaluating metrics for the train split
+# accuracy = accuracy_score(actual_labels, llm_first_word_responses)
+# precision = precision_score(actual_labels, llm_first_word_responses, average='micro')
+# recall = recall_score(actual_labels, llm_first_word_responses, average='micro')
+# f1 = f1_score(actual_labels, llm_first_word_responses, average='micro')
+# # roc_auc = roc_auc_score(actual_labels, llm_first_word_responses) # Uncomment if applicable
+
+# # Creating DataFrames for metrics
+# metrics = pd.DataFrame({'accuracy': [accuracy],
+#                         'precision': [precision],
+#                         'recall': [recall],
+#                         'f1_score': [f1]})
+#                         #'roc_auc': [roc_auc]}) # Uncomment if applicable
+
+# # Saving DataFrames to CSV files
+# metrics.to_csv(f'fpb_llama2_metrics_{config}_{today.strftime("%d_%m_%Y")}_{time_taken}.csv', index=False)
