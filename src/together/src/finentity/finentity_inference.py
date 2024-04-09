48235561d4258eb890f375380d04ed401434dcb0
Vivianyang0715 <yyangvivian@gmail.com>
2024-04-08 20:45:20 -0400

    finentity add sleep statement

--- src/together/src/finentity/finentity_inference.py
+++ src/together/src/finentity/finentity_inference.py
@@ -9,47 +9,54 @@ from prompts_and_tokens import tokens, finentity_prompt
 def finentity_inference(args):
     together.api_key = args.api_key
     today = date.today()
-    # OPTIONAL TODO: make configs an argument of some kind LOW LOW LOW PRIORITY
-    #configs = ["5768", "78516", "944601"]
     configs = ["5768"]
     
     for config in configs:
         dataset = load_dataset("gtfintechlab/finentity", config, token=args.hf_token)
 
-        # Initialize lists to store actual labels and model responses
-    sentences = []
-    llm_responses = []
-    llm_first_word_responses = []
-    actual_labels = []
-    complete_responses = []
+        sentences = []
+        llm_responses = []
+        actual_labels = []
+        complete_responses = []
 
-        # Iterating through the train split of the dataset
-    start_t = time.time()
-    for i in range(len(dataset['test'])):
-        sentence = dataset['test'][i]['content']
-        actual_label = dataset['train'][i]['annotations']
-        sentences.append(sentence)
-        actual_labels.append(actual_label)
-        try:
-            model_response = together.Complete.create(prompt=finentity_prompt(sentence),
-                            model=args.model,
-                            max_tokens=args.max_tokens,
-                            temperature=args.temperature,
-                            top_k=args.top_k,
-                            top_p=args.top_p,
-                            repetition_penalty=args.repetition_penalty,
-                            stop=tokens(args.model)
-                            )
-            complete_responses.append(model_response)
-            response_label = model_response["output"]["choices"][0]["text"]
-            llm_responses.append(response_label)
-            df = pd.DataFrame(
-                {'sentences': sentences, 'llm_responses': llm_responses, 'actual_labels': actual_labels, 'complete_responses': complete_responses}
-                )
-        except Exception as e:
-            print(e)
-            i = i - 1
+        for i in range(len(dataset['test'])):
+            sentence = dataset['test'][i]['content']
+            actual_label = dataset['test'][i]['annotations']  
+            sentences.append(sentence)
+            actual_labels.append(actual_label)
+            success = False
+
+            while not success:
+                try:
+                    model_response = together.Complete.create(
+                        prompt=finentity_prompt(sentence),
+                        model=args.model,
+                        max_tokens=args.max_tokens,
+                        temperature=args.temperature,
+                        top_k=args.top_k,
+                        top_p=args.top_p,
+                        repetition_penalty=args.repetition_penalty,
+                        stop=tokens(args.model)
+                    )
+                    success = True  
+                    response_label = model_response["output"]["choices"][0]["text"]
+                    llm_responses.append(response_label)
+                    print(llm_responses)
+                    complete_responses.append(model_response)  
+                    
+                except Exception as e:
+                    print(e)
+                    time.sleep(10.0)  
+
             time.sleep(10.0)
-            
-    return df
 
+        df = pd.DataFrame({
+            'sentences': sentences,
+            'llm_responses': llm_responses,
+            'actual_labels': actual_labels,
+            'complete_responses': complete_responses 
+        })
+        
+        
+
+    return df
