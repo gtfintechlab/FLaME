efa021fd9ef0dd53744a02fc1d045e097e79ef18
Huzaifa Pardawala <huzaifahp7@gmail.com>
2024-04-14 06:16:07 -0400

    Added metrics to inference

--- src/together/src/inference.py
+++ src/together/src/inference.py
@@ -1,131 +1,81 @@
 import argparse
+import re
+import numpy as np
+import pandas as pd
 from time import time
 from datetime import date
-import pandas as pd
-import together
-import task_specific_inference
-# from fpb.fpb_inference import fpb_inference
-# from finentity.finentity_inference import finentity_inference
+from tasks_inferences import fpb_inference, fomc_inference, numclaim_inference
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-from numclaim.numclaim_inference import numclaim_inference
-
-from sklearn.metrics import (
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score,
-    roc_auc_score,
-)
-
-# from utils.prompt_generator import (
-#     numclaim_prompt,
-#     fomc_prompt,
-#     finer_prompt,
-#     fpb_prompt,
-#     finentity_prompt,
-#     finqa_prompt,
-#     ectsum_prompt,
-#     banking77_prompt,
-#     convfinqa_prompt,
-# )
-
-today = date.today() 
-
-# task_prompt_map = {
-#     "numclaim": numclaim_prompt,
-#     "fomc": fomc_prompt,
-#     "finer": finer_prompt,
-#     "fpb": fpb_prompt,
-#     "finentity": finentity_prompt,
-#     "finqa": finqa_prompt,
-#     "ectsum": ectsum_prompt,
-#     "banking77": banking77_prompt,
-#     "convfinqa": convfinqa_prompt,
-# }
-
-task_generation_map = {
-    # "fpb": fpb_inference
-    "numclaim": numclaim_inference,
-    # "fomc": fomc_inference,
-    # "finer": finer_inference,
-    # "finentity": finentity_inference,
-    # "finqa": finqa_inference,
-    # "ectsum": ectsum_inference,
-    # "banking77": banking77_inference,
-    # "convfinqa": convfinqa_inference,
-}
-
+from task_specific_inference import numclaim_inference
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="Run a LLM on TogetherAI over the SuperFLUE dataset"
-    )
+    parser = argparse.ArgumentParser(description="Run a LLM on TogetherAI over the SuperFLUE dataset")
     parser.add_argument("--model", type=str, help="Model to use")
     parser.add_argument("--task", type=str, help="Task to use")
     parser.add_argument("--api_key", type=str, help="API key to use")
     parser.add_argument("--hf_token", type=str, help="Hugging Face token to use")
-    parser.add_argument("--max_tokens", type=int, help="Max tokens to use, default = 128")
-    parser.add_argument("--temperature", type=float, help="Temperature to use")
-    parser.add_argument('--top_p', type=float, help='Top-p to use, default = 0.7')
-    parser.add_argument("--top_k", type=int, help="Top-k to use, default = 50")
-    parser.add_argument('--repetition_penalty', type=float, help='Repetition penalty to use, default = 1.1')
-    args = parser.parse_args()
-    return args
+    parser.add_argument("--max_tokens", type=int, default=128, help="Max tokens to use")
+    parser.add_argument("--temperature", type=float, default=0.7, help="Temperature to use")
+    parser.add_argument("--top_p", type=float, default=0.7, help="Top-p to use")
+    parser.add_argument("--top_k", type=int, default=50, help="Top-k to use")
+    parser.add_argument("--repetition_penalty", type=float, default=1.1, help="Repetition penalty to use")
+    return parser.parse_args()
 
-def evaluate(df):
-    # Evaluating metrics for the train split
-    accuracy = accuracy_score(df['actual_labels'], df['llm_first_word_responses'])
-    precision = precision_score(df['actual_labels'], df['llm_first_word_responses'], average='micro')
-    recall = recall_score(df['actual_labels'], df['llm_first_word_responses'], average='micro')
-    f1 = f1_score(df['actual_labels'], df['llm_first_word_responses'], average='micro')
-    # roc_auc = roc_auc_score(actual_labels, llm_first_word_responses) # Uncomment if applicable
+def extract_label(text, label_regex):
+    match = re.search(label_regex, text)
+    return match.group(1) if match else 'None'
 
-    # Creating DataFrames for metrics
-    metrics = pd.DataFrame({'accuracy': [accuracy],
-                            'precision': [precision],
-                            'recall': [recall],
-                            'f1_score': [f1]})
-                            #'roc_auc': [roc_auc]}) # Uncomment if applicable
+def evaluate(file, response_column, label_regex, label_mapping):
+    data = pd.read_csv(file, index_col=0)
+    data['extracted_label'] = data[response_column].apply(lambda x: extract_label(x, label_regex))
+    data['extracted_label_numeric'] = data['extracted_label'].map(label_mapping)
+    data = data.dropna(subset=['extracted_label_numeric'])
 
+    metrics = {
+        'accuracy': accuracy_score(data['actual_label'], data['extracted_label_numeric']),
+        'precision': precision_score(data['actual_label'], data['extracted_label_numeric'], average='micro'),
+        'recall': recall_score(data['actual_label'], data['extracted_label_numeric'], average='micro'),
+        'f1_score': f1_score(data['actual_label'], data['extracted_label_numeric'], average='micro')
+    }
+
+    output_file_path = file.replace('.csv', '_metrics.csv')
+    pd.DataFrame([metrics]).to_csv(output_file_path, index=False)
     return metrics
 
 def main():
     args = parse_arguments()
-    # Trim and remove potential unexpected quotation marks from the task argument
     task = args.task.strip('“”"')
+
+    task_label_mapping = {
+        'fpb': {'POSITIVE': 2, 'NEGATIVE': 0, 'NEUTRAL': 1, 'None': np.nan},
+        'fomc': {'DOVISH': 0, 'HAWKISH': 1, 'NEUTRAL': 2, 'None': np.nan}
+    }
     
-    if task in task_generation_map:
-        inference_function = task_generation_map[task]
+    task_regex = {
+        'fpb': r'Label: (POSITIVE|NEGATIVE|NEUTRAL)',
+        'fomc': r'Label: (DOVISH|HAWKISH|NEUTRAL)',
+        'numclaim': r'Label: (OUTOFCLAIM|INCLAIM)'
+    }
+
+    task_inference_map = {
+        'numclaim': numclaim_inference,
+        'fpb': fpb_inference,
+        'fomc': fomc_inference,
+    }
+
+    if task in task_inference_map:
         start_t = time()
+        inference_function = task_inference_map[task]
         df = inference_function(args)
         time_taken = time() - start_t
-        # df.to_csv(
-        #    f'../{task}/results/{args.model}/{task}_{args.model}_{today.strftime("%d_%m_%Y")}_{time_taken}.csv', index=False
-       # )
+        df.to_csv(f'../{task}/results/{args.model}/{task}_{args.model}_{date.today().strftime("%d_%m_%Y")}_{time_taken}.csv', index=False)
         
+        metrics = evaluate(df, 'response', task_regex[task], task_label_mapping[task])
+        print(metrics)
+
     else:
         print(f"Task '{task}' not found in the task generation map.")
 
 if __name__ == "__main__":
     main()
-
-# def main():
-#     args = parse_arguments()
-#     inference_function = task_generation_map[args.task]
-#     start_t = time()
-#     df = inference_function(args)
-#     time_taken = time() - start_t
-#     df.to_csv(
-#         f'../{args.task}/results/{args.model}/{args.task}_{args.model}_{today.strftime("%d_%m_%Y")}_{time_taken}.csv', index=False
-#     )
-#     # TODO: CALCULATE METRICS USING DF
-#     # model, task = args.model, args.task
-    
-#     # df_metrics = evaluate(df)
-#     #  df.to_csv(
-#     #      f'../{args.task}/results/{args.model}/metrics_{args.task}_{args.model}_{today.strftime("%d_%m_%Y")}_{time_taken}.csv', index=False
-#     #)
-
-
-# if __name__ == "__main__":
-#     main()
