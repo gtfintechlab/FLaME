fde7d4297f74d586fe6c067dd38df98295441fdc
Huzaifa Pardawala <huzaifahp7@gmail.com>
2024-07-23 01:31:28 +0400

    feat: adding code to huggify fnxl (without splits)

---
+++ data/FNXL/huggify_fnxl.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from pathlib import Path
+from huggingface_hub import login
+import pandas as pd
+from datasets import Dataset, DatasetDict, load_dataset
+import logging
+
+
+DATA_DIRECTORY = Path().cwd().resolve().parent.parent / "data"
+SRC_DIRECTORY = Path().cwd().resolve().parent
+if str(SRC_DIRECTORY) not in sys.path:
+    sys.path.insert(0, str(SRC_DIRECTORY))
+
+
+HF_TOKEN = os.environ["HF_TOKEN"]
+HF_ORGANIZATION = "gtfintechlab"
+DATASET = "FNXL"
+login(HF_TOKEN)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def huggify_fnxl(push_to_hub=False, TASK=None):
+    try:
+        directory_path = DATA_DIRECTORY / "FNXL"
+        logger.debug(f"Directory path: {directory_path}")
+
+        dataset = load_dataset("TheFinAI/flare-fnxl")
+
+        test_set = dataset["test"] if "test" in dataset else []
+        
+        hf_dataset = DatasetDict()
+    
+
+        # Add test split
+        hf_dataset['test'] = test_set
+        
+
+        # Push to HF Hub
+        if push_to_hub:
+            hf_dataset.push_to_hub(
+                f"{HF_ORGANIZATION}/{DATASET}",
+                config_name= "main",
+                private=True,
+                token=HF_TOKEN,
+            )
+
+        logger.info(f"Finished processing FNXL")
+        return hf_dataset
+
+    except Exception as e:
+        logger.error(f"Error processing FNXL dataset: {str(e)}")
+        raise e
+
+
+if __name__ == "__main__":
+    TASK = "FNXL"
+
+    huggify_fnxl(push_to_hub=True, TASK=TASK)
