c07347b5b6a7553fcc9d519f9475e9c14f5d4e38
glennmatlin <glennmatlin@users.noreply.github.comw>
2024-09-27 20:45:17 -0400

    inference.py

---
+++ TODOs.md
@@ -0,0 +1,5 @@
+# TODOs
+
+## Glenn
+- `inference.py` should not be in the `together_code` directory ... unless it's not intended to be the entrypoint
+- if we maked `superflue.py` the entrypoint for starting the package that makes sense -- in which case the `inference.py` should stay where it is
--- src/together_code/inference.py
+++ src/together_code/inference.py
@@ -1,23 +1,6 @@
-import argparse
-import logging
 import sys
-import os
-from datetime import date
 from pathlib import Path
-from time import time
-
-import yaml
-
-from banking77.banking77_inference import banking77_inference
-from finbench.finbench_inference import finbench_inference
-from finentity.finentity_inference import finentity_inference
-from finer.finer_inference import finer_inference
-from fomc.fomc_inference import fomc_inference
-from fpb.fpb_inference import fpb_inference
-from numclaim.numclaim_inference import numclaim_inference
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from causal_detection.cd_inference import causal_detection_inference
-
+import logging
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 LOG_DIR = ROOT_DIR / 'logs'
 SRC_DIRECTORY = ROOT_DIR / 'src'
@@ -25,105 +8,95 @@ DATA_DIRECTORY = ROOT_DIR / 'data'
 OUTPUT_DIR = DATA_DIRECTORY / 'outputs'
 if str(SRC_DIRECTORY) not in sys.path:
     sys.path.insert(0, str(SRC_DIRECTORY))
-
+    
+import argparse
+from time import time
+from datetime import date
+from fpb.fpb_inference import fpb_inference
+from numclaim.numclaim_inference import numclaim_inference
+from fnxl.fnxl_inference import fnxl_inference
+from fomc.fomc_inference import fomc_inference
+from finbench.finbench_inference import finbench_inference
+from finer.finer_inference import finer_inference
+from finentity.finentity_inference import finentity_inference
+from headlines.headlines_inference import headlines_inference
+from finqa.fiqa_task1_inference import fiqa_inference
+from finqa.fiqa_task2_inference import fiqa_task2_inference
+from edtsum.edtsum_inference import edtsum_inference
 from src.utils.logging_utils import setup_logger
+from time import time
 
-logging.basicConfig(level=logging.DEBUG)
-logger = setup_logger(name="main_inference", log_file = LOG_DIR / "main_inference.log", level=logging.DEBUG)
-
-from src.utils.api_utils import make_api_call, save_raw_output
-from src.utils.logging_utils import setup_logger
+logger = setup_logger(name="together_inference", log_file = LOG_DIR / "together_inference.log", level=logging.DEBUG)
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="Run a LLM on TogetherAI over the SuperFLUE dataset"
-    )
+    parser = argparse.ArgumentParser(description="Run a LLM on TogetherAI over the SuperFLUE dataset")
     parser.add_argument("--model", type=str, help="Model to use")
     parser.add_argument("--task", type=str, help="Task to use")
     parser.add_argument("--api_key", type=str, help="API key to use")
     parser.add_argument("--hf_token", type=str, help="Hugging Face token to use")
-    with open(SRC_DIRECTORY/'config.yaml', "r") as file:
-        config = yaml.safe_load(file)
-        # parser.add_argument("--model", type=str, help="Model to use")
-        # parser.add_argument("--task", type=str, help="Task to use")
-        # parser.add_argument("--api_key", type=str, help="API key to use")
-        # parser.add_argument("--hf_token", type=str, help="Hugging Face token to use")
-        parser.add_argument(
-            "--max_tokens",
-            type=int,
-            default=config["fpb"]["max_tokens"],
-            help="Max tokens to use",
-        )
-        parser.add_argument(
-            "--temperature",
-            type=float,
-            default=config["fpb"]["temperature"],
-            help="Temperature to use",
-        )
-        parser.add_argument(
-            "--top_p", type=float, default=config["fpb"]["top_p"], help="Top-p to use"
-        )
-        parser.add_argument(
-            "--top_k", type=int, default=config["fpb"]["top_k"], help="Top-k to use"
-        )
-        parser.add_argument(
-            "--repetition_penalty",
-            type=float,
-            default=config["fpb"]["repetition_penalty"],
-            help="Repetition penalty to use",
-        )
-        parser.add_argument(
-            "--prompt_format",
-            type=str,
-            default="superflue",
-            help="Version of the prompt to use",
-        )
+
+    parser.add_argument(
+        "--max_tokens",
+        type=int,
+        default=128,
+        help="Max tokens to use",
+    )
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=0.0,
+        help="Temperature to use",
+    )
+    parser.add_argument(
+        "--top_p", type=float, default=0.9, help="Top-p to use"
+    )
+    # parser.add_argument(
+    #     "--top_k", type=int, default=50, help="Top-k to use"
+    # )
+    parser.add_argument(
+        "--repetition_penalty",
+        type=float,
+        default=1.0,
+        help="Repetition penalty to use",
+    )
+    parser.add_argument(
+        "--prompt_format",
+        type=str,
+        default="superflue",
+        help="Version of the prompt to use",
+    )
     return parser.parse_args()
-
-
-def process_api_response(results, task, model):
-    save_raw_output(results, task, model, OUTPUT_DIR)
-    return results
-
 
 def main():
     args = parse_arguments()
-    task = args.task.strip('"')
+    task = args.task.strip('“”"')
 
     task_inference_map = {
-        "numclaim": numclaim_inference,
-        "fpb": fpb_inference,
-        "fomc": fomc_inference,
-        "finbench": finbench_inference,
-        "finer": finer_inference,
-        "finentity": finentity_inference,
-        "finred": finred_inference,
-        "banking77": banking77_inference,
-        "causal_detection": causal_detection_inference,
-        "causal_classification": causal_classification_inference,
+        'numclaim': numclaim_inference,
+        'fpb': fpb_inference,
+        'fomc': fomc_inference,
+        'finbench': finbench_inference,
+        'finer': finer_inference,
+        'finentity': finentity_inference,
+        'headlines': headlines_inference,
+        'fiqa_task1': fiqa_inference, # double check this i think it might be _task1_
+        'fiqa_task2' : fiqa_task2_inference,
+        'edt_sum':edtsum_inference,
+        'fnxl': fnxl_inference,
     }
 
     if task in task_inference_map:
         start_t = time()
         inference_function = task_inference_map[task]
-        df = inference_function(args, make_api_call, process_api_response)
+        df = inference_function(args)
         time_taken = time() - start_t
-        logger.info(f"Time taken: {time_taken:.2f} seconds")
-        results_path = (
-            OUTPUT_DIR
-            / task
-            / args.model
-            / f"{task}_{args.model}_{date.today().strftime('%d_%m_%Y')}.csv"
-        )
+        print(time_taken)
+        results_path = ROOT_DIR / 'results' / task  / f"{task}_{args.model}_{date.today().strftime('%d_%m_%Y')}.csv"
         results_path.parent.mkdir(parents=True, exist_ok=True)
         df.to_csv(results_path, index=False)
         logger.info(f"Inference completed for {task}. Results saved to {results_path}")
-        # metrics = evaluate(df, 'response', task_regex[task], task_label_mapping[task])
-        # print(metrics)
-
     else:
-        logger.error(f"Task '{task}' not found in the task generation map.")
-
+        print(f"Task '{task}' not found in the task generation map.")
 
 if __name__ == "__main__":
     main()
