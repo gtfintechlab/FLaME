9e5d62b2f46cf5a26630c83a3537db03e10b275f
Huzaifa Pardawala <huzaifahp7@gmail.com>
2024-10-02 10:57:25 -0400

    fix: movig test file for inference to a different dir

--- results/causal_classification/test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/Users/hp/Desktop/SuperFLUE/results/causal_classification/causal_classification_meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo_02_10_2024.csv')
-
-print(df['llm_responses'][0])
