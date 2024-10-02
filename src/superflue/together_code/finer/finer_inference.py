d55f352c30c29a36c33944258bbee726ef4d2ea1
Huzaifa Pardawala <huzaifahp7@gmail.com>
2024-10-02 03:17:26 -0400

    feat(finer): editing script for finer and running it

--- src/superflue/together_code/finer/finer_inference.py
+++ src/superflue/together_code/finer/finer_inference.py
@@ -38,7 +38,7 @@ def finer_inference(args):
         sentences.append(sentence)
         actual_labels.append(actual_label)
         try:
-            
+            logger.debug(f"Processing sentence {i+1}/{len(dataset['test'])}") # type: ignore
             model_response = client.chat.completions.create(
             model=args.model,
             messages=[{"role": "user", "content": finer_prompt(sentence)}],
