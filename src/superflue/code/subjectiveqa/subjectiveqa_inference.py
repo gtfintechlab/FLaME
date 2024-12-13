336452dec45b254e2bf245dcb46b44a8023bd933
Huzaifa Pardawala <huzaifahp7@gmail.com>
2024-12-13 15:32:27 -0800

    feat: refactroing subjectiveqa's script

--- src/superflue/code/subjectiveqa/subjectiveqa_inference.py
+++ src/superflue/code/subjectiveqa/subjectiveqa_inference.py
@@ -2,10 +2,8 @@ import time
 from datetime import date
 import pandas as pd
 from datasets import load_dataset
-
 from litellm import batch_completion 
-from superflue.code.prompts import subjectiveqa_prompt
-from superflue.code.tokens import tokens
+from superflue.code.prompts_oldsuperflue import subjectiveqa_prompt
 from superflue.utils.logging_utils import setup_logger
 from superflue.config import LOG_LEVEL, LOG_DIR, RESULTS_DIR
 from superflue.utils.batch_utils import chunk_list, process_batch_with_retry
