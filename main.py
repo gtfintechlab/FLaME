1e1b30fb3376147e52a77301a77cff15530d4381
Glenn Matlin <glennmatlin@users.noreply.github.com>
2025-05-03 17:51:01 -0400

    Remove debug prints and old requirements file

--- main.py
+++ main.py
@@ -49,14 +49,8 @@ def parse_arguments():
 
 
 if __name__ == "__main__":
-    # Load environment variables first
     load_dotenv()
-
-    # Optional: Verify that environment variables are loaded
-    print(f"TOGETHER_API_KEY: {os.getenv('TOGETHERAI_API_KEY')}")
-    print(f"HUGGINGFACEHUB_API_TOKEN: {os.getenv('HUGGINGFACEHUB_API_TOKEN')}")
     HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-    # Log in to Hugging Face if the token is set
     if HUGGINGFACEHUB_API_TOKEN:
         login(token=HUGGINGFACEHUB_API_TOKEN)
     else:
@@ -64,7 +58,6 @@ if __name__ == "__main__":
             "Hugging Face API token not found. Please set HUGGINGFACEHUB_API_TOKEN in the environment."
         )
 
-    # Now import the inference function
     args = parse_arguments()
 
     with open(args.config, "r") as file:
